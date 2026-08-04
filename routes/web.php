27efbeeec46988ad41f39e02968f53ebb0ef15f6
muhammadshaharyaraulakh <muhammadshaharyaraulakh@gmail.com>
2026-08-04 16:53:52 +0500

    Route Protected by Middlewares

--- routes/web.php
+++ routes/web.php
@@ -5,115 +5,110 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
+/*
+|--------------------------------------------------------------------------
+| Public Route (No Middleware)
+|--------------------------------------------------------------------------
+*/
 Route::get('/', [CompanyController::class, 'index'])->name('home');
 
-Route::get('/terms', function () {
-    return Inertia::render('TermsOfService');
-})->name('TermsOfService');
+/*
+|--------------------------------------------------------------------------
+| Admin Protected Routes
+|--------------------------------------------------------------------------
+*/
+Route::middleware(['auth', 'admin'])->group(function () {
+    Route::get('/AdminDashboard', function () {
+        return Inertia::render('AdminDashboard');
+    })->name('AdminDashboard');
+});
 
-Route::get('/ComingSoon', function () {
-    return Inertia::render('ComingSoon');
-})->name('ComingSoon');
+/*
+|--------------------------------------------------------------------------
+| Company Protected Routes
+|--------------------------------------------------------------------------
+*/
+Route::middleware(['auth', 'company'])->group(function () {
+    Route::get('/CompanyDashboard', function () {
+        return Inertia::render('CompanyDashboard');
+    })->name('CompanyDashboard');
 
-Route::get('/Faq', function () {
-    return Inertia::render('Faq');
-})->name('Faq');
+    Route::get('/CompanyProfile', function () {
+        return Inertia::render('CompanyProfile');
+    })->name('CompanyProfile');
+});
 
-Route::get('/Contact', function () {
-    return Inertia::render('Contact');
-})->name('Contact');
+/*
+|--------------------------------------------------------------------------
+| Candidate Protected Routes
+|--------------------------------------------------------------------------
+*/
+Route::middleware(['auth', 'candidate'])->group(function () {
+    Route::get('/CandidateDashboard', function () {
+        return Inertia::render('CandidateDashboard');
+    })->name('CandidateDashboard');
+});
 
-Route::get('/find-job', function () {
-    return Inertia::render('FindJob');
-})->name('find.job');
-
-Route::get('/FindJob', function () {
-    return Inertia::render('FindJob');
-})->name('FindJob');
-
-Route::get('/job-details/{id?}', function () {
-    return Inertia::render('JobDetails');
-})->name('job.details');
-
-Route::get('/JobDetails', function () {
-    return Inertia::render('JobDetails');
-})->name('JobDetails');
-
-Route::get('/find-employers', function () {
-    return Inertia::render('FindEmployers');
-})->name('find.employers');
-
-Route::get('/FindEmployers', function () {
-    return Inertia::render('FindEmployers');
-})->name('FindEmployers');
-
-Route::get('/company-details/{id?}', function () {
-    return Inertia::render('CompanyDetails');
-})->name('company.details');
-
-Route::get('/CompanyDetails', function () {
-    return Inertia::render('CompanyDetails');
-})->name('CompanyDetails');
-
-Route::get('/About', function () {
-    return Inertia::render('About');
-})->name('About');
-
-Route::get('/blogs', function () {
-    return Inertia::render('AllBlogs');
-})->name('blogs');
-
-Route::get('/AllBlogs', function () {
-    return Inertia::render('AllBlogs');
-})->name('AllBlogs');
-
-Route::get('/blog/{id?}', function () {
-    return Inertia::render('SingleBlog');
-})->name('SingleBlog');
-
-Route::get('/company-profile', function () {
-    return Inertia::render('CompanyProfile');
-})->name('company.profile');
-
-Route::get('/CompanyProfile', function () {
-    return Inertia::render('CompanyProfile');
-})->name('CompanyProfile');
-
-Route::get('/CompanyDashboard', function () {
-    return Inertia::render('CompanyDashboard');
-})->name('CompanyDashboard');
-
-Route::get('/company-dashboard', function () {
-    return Inertia::render('CompanyDashboard');
-})->name('company.dashboard');
-
-Route::get('/dashboard', function () {
-    return Inertia::render('CompanyDashboard');
-})->name('dashboard');
-
-Route::get('/CandidateDashboard', function () {
-    return Inertia::render('CandidateDashboard');
-})->name('CandidateDashboard');
-
-Route::get('/candidate-dashboard', function () {
-    return Inertia::render('CandidateDashboard');
-})->name('candidate.dashboard');
-
-Route::get('/AdminDashboard', function () {
-    return Inertia::render('AdminDashboard');
-})->name('AdminDashboard');
-
-Route::get('/admin-dashboard', function () {
-    return Inertia::render('AdminDashboard');
-})->name('admin.dashboard');
-
-
+/*
+|--------------------------------------------------------------------------
+| General Authenticated User Routes (Require 'auth' Middleware)
+|--------------------------------------------------------------------------
+*/
 Route::middleware('auth')->group(function () {
+    Route::get('/FindJob', function () {
+        return Inertia::render('FindJob');
+    })->name('FindJob');
+
+    Route::get('/JobDetails/{id?}', function () {
+        return Inertia::render('JobDetails');
+    })->name('JobDetails');
+
+    Route::get('/FindEmployers', function () {
+        return Inertia::render('FindEmployers');
+    })->name('FindEmployers');
+
+    Route::get('/CompanyDetails/{id?}', function () {
+        return Inertia::render('CompanyDetails');
+    })->name('CompanyDetails');
+
+    Route::get('/AllBlogs', function () {
+        return Inertia::render('AllBlogs');
+    })->name('AllBlogs');
+
+    Route::get('/SingleBlog/{id?}', function () {
+        return Inertia::render('SingleBlog');
+    })->name('SingleBlog');
+
+    Route::get('/About', function () {
+        return Inertia::render('About');
+    })->name('About');
+
+    Route::get('/Contact', function () {
+        return Inertia::render('Contact');
+    })->name('Contact');
+
+    Route::get('/Faq', function () {
+        return Inertia::render('Faq');
+    })->name('Faq');
+
+    Route::get('/TermsOfService', function () {
+        return Inertia::render('TermsOfService');
+    })->name('TermsOfService');
+
+    Route::get('/ComingSoon', function () {
+        return Inertia::render('ComingSoon');
+    })->name('ComingSoon');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+/*
+|--------------------------------------------------------------------------
+| Error & Fallback Routes
+|--------------------------------------------------------------------------
+*/
 Route::get('/404', function () {
     return Inertia::render('NotFound');
 })->name('NotFound');
